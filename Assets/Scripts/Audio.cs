6eb9ce9dd7a870d1cb8b34b4a50775a0cceb734c
Presac\Lenovo <casperhahn@hotmail.com>
2017-05-17 10:27:35 +0200

    First push

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    AudioSource[] audio;
+    int CurrentClip = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        audio = GetComponents<AudioSource>();
+    }
+
+    void Update()
+    {
+        Debug.Log(audio[CurrentClip].time + " out of " + audio[CurrentClip].clip.length);
+    }
+
+    public void PlayAudio()
+    {
+        audio[CurrentClip].Play();
+    }
+
+    public void PauseAudio()
+    {
+        audio[CurrentClip].Pause();
+    }
+
+    public void PlayFromPoint()
+    {
+        audio[CurrentClip].Play();
+        //audio[CurrentClip].time = 2.0f;
+    }
+}
---
+++ Assets/Scripts/PlayerPauser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPauser : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    /*public void PlaySong()
+    {
+        if (audioClip != null  AudioSourceMusic != null)
+        {
+            AudioSourceMusic.loop = true;
+            AudioSourceMusic.volume = 1;
+            AudioSourceMusic.clip = audioClip;
+            AudioSourceMusic.Play();
+        }
+    }
+
+    public void PauseMusic()
+    {
+        if (AudioSourceMusic != null)
+            AudioSourceMusic.Pause();
+    }
+
+    public void UnPauseMusic()
+    {
+        if (AudioSourceMusic != null)
+            AudioSourceMusic.Play();
+    }*/
+}
